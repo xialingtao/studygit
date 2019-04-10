38024c314fae9c497ebf18e77a9d285c94c12bfc
whogjy <gongjianye@qiakr.com>
2019-04-10 12:43:35 +0800

    初始化文件

---
+++ src/test/java/com/gitstudy/GitstudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gitstudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitstudyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
